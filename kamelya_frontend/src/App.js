aa25059856d6010c68ca1acc791e234a1743f08e
Said Berk <csaidberk@gmail.com>
2023-09-28 11:59:31 +0300

    Routes defined.

--- kamelya_frontend/src/App.js
+++ kamelya_frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import {Routes, Route, useNavigate } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Routes>
+      <Route path = "login" element = {<Login />} />
+      <Route path = "/*" element = {<Home />} />
+    </Routes>
   );
 }
 
